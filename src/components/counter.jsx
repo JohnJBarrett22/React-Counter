03fde1d70a360f78867deaea1038529183e4c4cf
John Barrett <johnjbarrett22@gmail.com>
2019-03-19 02:05:43 -0700

    Working through the tutorial with various changes as I learn about React, saving here as I am fleshing out the counter now

---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="bn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
