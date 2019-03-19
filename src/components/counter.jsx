54f8b2cd31b8c085fd79de749a454c05ce7374f4
John Barrett <johnjbarrett22@gmail.com>
2019-03-19 15:40:07 -0700

    Modified render functions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -30,8 +30,8 @@ class Counter extends Component {
     );
   }
 
-  handleIncrement = () => {
-    console.log("Increment Clicked", this);
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -40,7 +40,7 @@ class Counter extends Component {
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
