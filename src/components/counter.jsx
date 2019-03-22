565c10490a22b6eee9b7b7619cae1f8cd332dae5
John Barrett <johnjbarrett22@gmail.com>
2019-03-22 14:27:09 -0700

    Removed local state from counter.jsx and nested all control under counters.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,46 +1,12 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-    imageUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"]
-  };
-
-  styles = {
-    fontSize: 10,
-    fontWeight: "bold"
-  };
-
-  //   constructor() {
-  //     super();
-  //     this.handleIncrement = this.handleIncrement.bind(this);
-  //     console.log("Constructor", this);
-  //   }
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}>{tag}</li>
-        ))}
-      </ul>
-    );
-  }
-
-  handleIncrement = product => {
-    console.log(product);
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({ id: 1 })}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -51,19 +17,18 @@ class Counter extends Component {
         >
           Delete
         </button>
-        {/* {this.renderTags()} */}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 }
